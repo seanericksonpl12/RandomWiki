fb84a35d25a9d78a6802ddfba138ff19ab2f4114
Sean <seanericksonpl12@gmail.com>
2022-08-26 18:53:43 -0500

    Initial Commit

---
+++ RandomWiki/RandomWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWikiApp.swift
+//  RandomWiki
+//
+//  Created by Sean Erickson on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
