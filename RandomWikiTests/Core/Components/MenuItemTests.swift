57604c167a43f4071d3483e5394e6d898caeb2c6
seanericksonpl12 <seanericksonpl12@gmail.com>
2022-09-19 14:19:15 -0500

    Added MenuItem tests

---
+++ RandomWikiTests/Core/Components/MenuItemTests.swift
@@ -0,0 +1,36 @@
+//
+//  MenuItemViewTests.swift
+//  RandomWikiTests
+//
+//  Created by Sean Erickson on 9/19/22.
+//
+
+import XCTest
+import ViewInspector
+@testable import RandomWiki
+import SwiftUI
+
+extension MenuItem: Inspectable {}
+
+class MenuItemTests: XCTestCase {
+    
+    var view: MenuItem<EmptyView>!
+    
+    override func setUp() {
+        super.setUp()
+        view = MenuItem(iconName: "TestIcon", iconColor: .black, title: "Test", destination: {EmptyView()})
+        continueAfterFailure = false
+    }
+    
+    func testTitle() {
+        do {
+            try view.inspect().find(text: "Test")
+        } catch { XCTFail() }
+    }
+    
+    func testImage() {
+        do {
+            try view.inspect().findAll(ViewType.Image.self, where: { try $0.actualImage().name() == "TestIcon"})
+        } catch { XCTFail() }
+    }
+}
