310a3f97cf2386f97ee6971c0ef7904a92fcf2ba
seanericksonpl12 <seanericksonpl12@gmail.com>
2023-07-08 22:14:09 -0500

    Core data save bug fix

--- RandomWiki/Modules/Home/ContentViewModel.swift
+++ RandomWiki/Modules/Home/ContentViewModel.swift
@@ -84,10 +84,8 @@ extension ContentView {
         func saveCoreData(_ save: Article, context: NSManagedObjectContext, list: FetchedResults<ArticleModel>) {
             do {
                 // Check if item is already in favorites list
-                if save.saved {
-                    if let item = list.first(where: { $0.id == save.id}) {
-                        context.delete(item)
-                    } else { print("Error: Duplicate item not found") }
+                if let item = list.first(where: { $0.id == save.id}) {
+                    context.delete(item)
                 }
                 else {
                     let article = ArticleModel(context: context)
